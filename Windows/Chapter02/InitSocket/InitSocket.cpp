e38c0a45e04f518575cb11f28ab22f34512c67f3
ChamChiCham <kyjp0623@gmail.com>
2024-09-19 22:02:06 +0900

    Revert "Chapter2 과제"
    This reverts commit e3ce0f369cfd39b871db196decd933b754a4f104.

--- Windows/Chapter02/InitSocket/InitSocket.cpp
+++ Windows/Chapter02/InitSocket/InitSocket.cpp
@@ -1,44 +1,20 @@
 #include "..\..\Common.h"
-#include <iostream>
-#include <string>
-#include <format>
-
-constexpr WORD make_word(BYTE first, BYTE second) noexcept
-{
-	return (static_cast<WORD>(first) << 8) | static_cast<WORD>(second);
-}
-
-constexpr BYTE high_byte(WORD word) noexcept
-{
-	return static_cast<BYTE>(word >> 8);
-}
-
-constexpr BYTE low_byte(WORD word) noexcept
-{
-	return static_cast<BYTE>(word);
-}
-
-std::string word_to_version(WORD word) noexcept
-{
-	return std::format("{:d}.{:d}", high_byte(word), low_byte(word));
-}
 
 int main(int argc, char *argv[])
-{	
+{
 	// 윈속 초기화
 	WSADATA wsa;
-	if (WSAStartup(make_word(2, 2), &wsa) != 0)
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
 		return 1;
 	printf("[알림] 윈속 초기화 성공\n");
 
+	// 소켓 생성
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET) err_quit("socket()");
+	printf("[알림] 소켓 생성 성공\n");
 
-	// wsa 변수 필드 출력
-	std::cout
-		<< "wVersion: " << word_to_version(wsa.wVersion) << std::endl
-		<< "wHighVersion: " << word_to_version(wsa.wHighVersion) << std::endl
-		<< "szDescription: " << wsa.szDescription << std::endl
-		<< "szSystemStatus: " << wsa.szSystemStatus << std::endl;
-	
+	// 소켓 닫기
+	closesocket(sock);
 
 	// 윈속 종료
 	WSACleanup();
